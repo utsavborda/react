611381ff473f877de86bf882a674e8a461ea0aef
utsavborda <bordautsav0@gmail.com>
2025-03-27 15:36:05 +0530

    Add files via upload

---
+++ News.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col md-3">
+            <NewsItem title="mytitle" description="mydsc" />
+          </div>
+          <div className="col md-3">
+            <NewsItem title="mytitle" description="mydsc" />
+          </div>
+          <div className="col md-3">
+            <NewsItem title="mytitle" description="mydsc" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
---
+++ NewsItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/3558/live/ebbf98c0-ee3e-11ef-8674-2fbdc08c7863.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
